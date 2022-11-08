f934cce10e5a7bc80a711ccf22474a7be01eb8b7
vishnukbgit <vishnukb91@yahoo.com>
2022-11-08 12:21:27 +0530

    json added

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM node:alpine
 RUN npm install snyk-to-html -g
-COPY ${WORKSPACE} /app 
-CMD snyk-to-html -i results.json -o results.html
+COPY ${WORKSPACE} /app
--- script.sh
+++ script.sh
@@ -4,6 +4,6 @@ SNYK_TOKEN="2286d438-4472-438c-9113-d6283e2c0a48"
 #cp swagger.json ${WORKSPACE}/out/swagger.json
 docker run --rm -i -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v ${WORKSPACE}:/app snyk/snyk:alpine snyk code test --json > snykresults.json
 docker build . -t snykhtml
-docker run snykhtml -v ${WORKSPACE}:/app
+docker run --rm -i snykhtml -v ${WORKSPACE}:/app snyk-to-html -i results.json -o results.html
 
 #cp ${WORKSPACE}/out/result.json ${WORKSPACE}/testreport.json
