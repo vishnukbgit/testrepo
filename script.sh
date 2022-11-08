267a5fe7725280990a0047ac56be5927d3535b5a
vishnukbgit <vishnukb91@yahoo.com>
2022-11-08 10:33:19 +0530

    files added

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ chmod 777 ${WORKSPACE}/out
 SNYK_TOKEN="2286d438-4472-438c-9113-d6283e2c0a48"
 #cp swagger.json ${WORKSPACE}/out/swagger.json
 docker run --rm -it -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v $(pwd):/app snyk/snyk:alpine snyk code test --json > results.json
-cp ${WORKSPACE}/out/testreport.html ${WORKSPACE}/testreport.html
+cp ${WORKSPACE}/out/result.json ${WORKSPACE}/testreport.json
