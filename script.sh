96af06bfa623c19e5066feafa54aa62d2e675ff5
vishnukbgit <vishnukb91@yahoo.com>
2022-11-08 15:16:18 +0530

    new

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-docker run --rm -i -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v $"`pwd`":/app snyk/snyk:alpine snyk code test --all-projects --json > snyktest.json
+docker run --rm -i -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v ${WORKSPACE}:/app snyk/snyk:alpine snyk code test --all-projects --json > snyktest.json
 #docker build . -t snykhtml
 #docker run --rm -i snykhtml -v ${WORKSPACE}:/app snyk-to-html -i results.json -o results.html
 
