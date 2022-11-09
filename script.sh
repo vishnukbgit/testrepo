0beff5f9352f046a2a0433410bb7ae52a128db04
vishnukbgit <vishnukb91@yahoo.com>
2022-11-09 10:01:53 +0530

    new file

--- script.sh
+++ script.sh
@@ -1,4 +1,6 @@
 pwd
+mkdir -p ${WORKSPACE}/out
+chmod 777 ${WORKSPACE}/out
 ls -la
-docker run --rm -i -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v ${/var/lib/jenkins/workspace}:/app snyk/snyk:alpine snyk code test --all-projects --json > snyktest.json
+docker run --rm -i -e  SNYK_TOKEN='2286d438-4472-438c-9113-d6283e2c0a48' -v ${WORKSPACE}/out:/app snyk/snyk:alpine snyk code test --all-projects --json > ${WORKSPACE}/out/snyktest.json
 pwd
